cd66d4c45ebc15a7153f2f25503cc0574dd5052d
Jesse Natalie <jenatali@microsoft.com>
2021-04-11 07:34:11 -0700

    Clean up #defines before including CL headers to prevent warnings

--- include/platform.hpp
+++ include/platform.hpp
@@ -6,6 +6,16 @@
 #define CL_USE_DEPRECATED_OPENCL_1_0_APIS
 #define CL_USE_DEPRECATED_OPENCL_1_1_APIS
 #define CL_USE_DEPRECATED_OPENCL_1_2_APIS
+#define CL_USE_DEPRECATED_OPENCL_2_0_APIS
+#define CL_USE_DEPRECATED_OPENCL_2_1_APIS
+#define CL_USE_DEPRECATED_OPENCL_2_2_APIS
+
+#ifdef CLON12_SUPPORT_3_0
+#define CL_TARGET_OPENCL_VERSION 300
+#else
+// Choosing 2.2 since we have stubs for everything currently
+#define CL_TARGET_OPENCL_VERSION 220
+#endif
 
 #include <D3D12TranslationLayerDependencyIncludes.h>
 #include <D3D12TranslationLayerIncludes.h>
